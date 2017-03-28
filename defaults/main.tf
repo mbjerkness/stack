f4b68f3a8873505ca6385b42ffd5649a59e5dbff
Mike Bjerkness <mike.bjerkness@centriam.com>
2017-03-28 14:06:35 -0500

    Revert the AMI updates. The update caused the EC2 instances to fail to register with the cluster. A new cluster called 'default' was created and then everything stopped working :(

--- defaults/main.tf
+++ defaults/main.tf
@@ -24,15 +24,15 @@ variable "cidr" {
 
 variable "default_ecs_ami" {
   default = {
-    us-east-1      = "ami-275ffe31"
-    us-west-1      = "ami-689bc208"
-    us-west-2      = "ami-62d35c02"
-    eu-west-1      = "ami-95f8d2f3"
-    eu-central-1   = "ami-085e8a67"
-    ap-northeast-1 = "ami-f63f6f91"
+    us-east-1      = "ami-dde4e6ca"
+    us-west-1      = "ami-6d21770d"
+    us-west-2      = "ami-97da70f7"
+    eu-west-1      = "ami-c41f3bb7"
+    eu-central-1   = "ami-4ba16024"
+    ap-northeast-1 = "ami-90ea86f7"
     ap-northeast-2 = "ami-8a4b9ce4"
-    ap-southeast-1 = "ami-b4ae1dd7"
-    ap-southeast-2 = "ami-fbe9eb98"
+    ap-southeast-1 = "ami-d603afb5"
+    ap-southeast-2 = "ami-1ddce47e"
     sa-east-1      = "ami-29039a45"
   }
 }
