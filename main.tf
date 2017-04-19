83d77cfef40c4a27c5cbd7eb71910a07ff163cc8
Mike Bjerkness <mike@bjerkness.com>
2017-04-19 14:37:30 -0500

    Add environment to the names of resources (#2)
    * Add environment to the names of resources
    * move our updates to the web-service to it's own directory, web-service-alb
    
    From segment/master:
    * udpated ecs-agent to 1.14.1
    * output ecs task definition revision from task module

--- main.tf
+++ main.tf
@@ -47,16 +47,19 @@ variable "cidr" {
 
 variable "internal_subnets" {
   description = "a list of CIDRs for internal subnets in your VPC, must be set if the cidr variable is defined, needs to have as many elements as there are availability zones"
+  type = "list"
   default     = ["10.30.0.0/19" ,"10.30.64.0/19", "10.30.128.0/19"]
 }
 
 variable "external_subnets" {
   description = "a list of CIDRs for external subnets in your VPC, must be set if the cidr variable is defined, needs to have as many elements as there are availability zones"
+  type = "list"
   default     = ["10.30.32.0/20", "10.30.96.0/20", "10.30.160.0/20"]
 }
 
 variable "availability_zones" {
   description = "a comma-separated list of availability zones, defaults to all AZ of the region, if set to something other than the defaults, both internal_subnets and external_subnets have to be defined as well"
+  type = "list"
   default     = ["us-west-2a", "us-west-2b", "us-west-2c"]
 }
 
@@ -318,4 +321,9 @@ output "internal_route_tables" {
 // The external route table ID.
 output "external_route_tables" {
   value = "${module.vpc.external_rtb_id}"
+}
+
+// The internal ssh security group ID.
+output "internal_ssh_security_group" {
+  value = "${module.security_groups.internal_ssh}"
 }
