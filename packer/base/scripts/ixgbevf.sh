9543eecd372d72c80ce16a9b1693de5917fc020a
Mike Bjerkness <mike.bjerkness@centriam.com>
2018-03-01 15:17:55 -0600

    Add jumpcloud agent, update ecs-agent, upgrade networking driver

--- packer/base/scripts/ixgbevf.sh
+++ packer/base/scripts/ixgbevf.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e
 
-wget -q -N -P /tmp/ "sourceforge.net/projects/e1000/files/ixgbevf stable/3.1.2/ixgbevf-3.1.2.tar.gz"
-tar -xzf /tmp/ixgbevf-3.1.2.tar.gz
-mv ixgbevf-3.1.2 /usr/src/
+version=4.3.4
+wget -q -N -P /tmp/ "https://s3.amazonaws.com/cntrm-build-assets/ixgbevf-${version}.tar.gz"
+tar -xzf /tmp/ixgbevf-${version}.tar.gz
+mv ixgbevf-${version} /usr/src/
 
-cat <<EOT | tee /usr/src/ixgbevf-3.1.2/dkms.conf
+cat <<EOT | tee /usr/src/ixgbevf-${version}/dkms.conf
 PACKAGE_NAME="ixgbevf"
-PACKAGE_VERSION="3.1.2"
+PACKAGE_VERSION="${version}"
 CLEAN="cd src/; make clean"
 MAKE="cd src/; make BUILD_KERNEL=\${kernelver}"
 BUILT_MODULE_LOCATION[0]="src/"
@@ -17,9 +18,9 @@ DEST_MODULE_NAME[0]="ixgbevf"
 AUTOINSTALL="yes"
 EOT
 
-dkms add -m ixgbevf -v 3.1.2
-dkms build -m ixgbevf -v 3.1.2
-dkms install -m ixgbevf -v 3.1.2
+dkms add -m ixgbevf -v ${version}
+dkms build -m ixgbevf -v ${version}
+dkms install -m ixgbevf -v ${version}
 update-initramfs -c -k all
 
 echo "options ixgbevf InterruptThrottleRate=1,1,1,1,1,1,1,1" | tee /etc/modprobe.d/ixgbevf.conf
